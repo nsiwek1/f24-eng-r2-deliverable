6a0122083a303949336a46fa8bff9a5c7afc155a
Natalia <natalia@dhcp-10-250-20-23.harvard.edu>
2024-09-16 10:29:20 -0400

    fixes

--- app/species/page.tsx
+++ app/species/page.tsx
@@ -20,6 +20,7 @@ export default async function SpeciesList() {
   // Obtain the ID of the currently signed-in user
   const sessionId = session.user.id;
 
+  // Fetch species data
   const { data: species } = await supabase.from("species").select("*").order("id", { ascending: false });
 
   return (
@@ -30,7 +31,7 @@ export default async function SpeciesList() {
       </div>
       <Separator className="my-4" />
       <div className="flex flex-wrap justify-center">
-        {species?.map((species) => <SpeciesCard key={species.id} species={species} />)}
+        {species?.map((species) => <SpeciesCard key={species.id} species={species} userId={sessionId} />)}
       </div>
     </>
   );
